e65e8a8f49c82be4ad0668f3715a76ef632e4cbe
reececorkhill <reece@smartit-webdevelopment.com>
2024-01-14 22:04:59 +0000

    Added answer feedback for correct and wrong answers.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,34 +2,30 @@ var startButton = document.querySelector("#start");
 var questionsDisplay = document.querySelector("#questions");
 var currentQuestionIndex = 0;
 var timeLeft = 75;
+var score = 0;
 
-startButton.addEventListener("click", function() {
+const firstAnswer = document.getElementById("answer-1");
+const secondAnswer = document.getElementById("answer-2");
+const thirdAnswer = document.getElementById("answer-3");
+const fourthAnswer = document.getElementById("answer-4");
+
+firstAnswer.addEventListener("click", checkAnswer);
+secondAnswer.addEventListener("click", checkAnswer);
+thirdAnswer.addEventListener("click", checkAnswer);
+fourthAnswer.addEventListener("click", checkAnswer);
+startButton.addEventListener("click", startTheQuiz);
+
+function startTheQuiz() {
     timerCountdown();
-    // console.log("Button working!")
     const startScreenDisplay = document.getElementById("start-screen");
     startScreenDisplay.remove();
-    // console.log("Start-Screen block removed!")
     const showQuestions = document.getElementById("questions");
     showQuestions.classList.remove("hide");
-    // console.log("Questions are now showing?")
-    // console.log("Has timer started counting down?")
     showQuestionsAndAnswers (timeLeft, questionsAndAnswers[currentQuestionIndex]);
-        // showQuestionsAndAnswers (questionsAndAnswers[currentQuestionIndex]);
-        // displayAnswers(questionsAndAnswers[currentQuestionIndex]);
-        // displayCorrectAnswer(questionsAndAnswers[currentQuestionIndex]);
-        // currentQuestionIndex++;
-    // showQuestionsAndAnswers (questionsAndAnswers[currentQuestionIndex]);
-    // displayAnswers(questionsAndAnswers[currentQuestionIndex]);
-    // displayCorrectAnswer(questionsAndAnswers[currentQuestionIndex]);
-    checkAnswer(questionsAndAnswers[currentQuestionIndex].answerChoices, questionsAndAnswers[currentQuestionIndex].correctChoice);
-});
+}
 
 function timerCountdown() {
-    const timer = document.getElementById("time");
-    // console.log(timer)
-
-    var timeLeft = 75;
-
+    const timer = document.getElementById("time");   
     var timeDeduct = setInterval(function() {
         if (timeLeft > 0) {
             timer.textContent = timeLeft + ' seconds remaining';
@@ -43,8 +39,7 @@ function timerCountdown() {
 }
 
 function showQuestionsAndAnswers (time, test) {
-    console.log(test.question);
-
+    if (currentQuestionIndex+1 > questionsAndAnswers.length) return;
     const currentQuestion = document.getElementById("question-title");
     const firstAnswer = document.getElementById("answer-1");
     const secondAnswer = document.getElementById("answer-2");
@@ -53,62 +48,36 @@ function showQuestionsAndAnswers (time, test) {
 
     if (time > 0) {
         currentQuestion.textContent = test.question;
-        // console.log(test.answerChoices)
         firstAnswer.textContent = test.answerChoices[0];
         secondAnswer.textContent = test.answerChoices[1];
         thirdAnswer.textContent = test.answerChoices[2];
         fourthAnswer.textContent = test.answerChoices[3];
-      } else {
-        console.log("Test Log")
     };
 }
 
-function checkAnswer(choices, correctanswer) {
-    const firstAnswer = document.getElementById("answer-1");
-    const secondAnswer = document.getElementById("answer-2");
-    const thirdAnswer = document.getElementById("answer-3");
-    const fourthAnswer = document.getElementById("answer-4");
+function checkAnswer(event) {
+    const feedbackDiv = document.getElementById("feedback");
+    feedbackDiv.classList.remove("hide");
+    const showFeedback = document.getElementById("feedback-alert");
+    var choices = questionsAndAnswers[currentQuestionIndex].answerChoices;
+    var correctanswer = questionsAndAnswers[currentQuestionIndex].correctChoice;
+    var button = event.target;
 
-    console.log(choices);
-    console.log(correctanswer);
-    firstAnswer.addEventListener("click", function(){
-        if (parseInt(firstAnswer.getAttribute("data-index")) === choices.indexOf(correctanswer)) {
-            console.log("Correct Answer!");
-        } else {
-            console.log("Wrong!");
-            penalty = timeLeft - 10;
-        }  
-    });
-
-    secondAnswer.addEventListener("click", function(){
-        if (parseInt(secondAnswer.getAttribute("data-index")) === choices.indexOf(correctanswer)) {
-            console.log("Correct Answer!");
-        } else {
-            console.log("Wrong!");
-        }  
-    });
-
-    thirdAnswer.addEventListener("click", function(){
-        if (parseInt(thirdAnswer.getAttribute("data-index")) === choices.indexOf(correctanswer)) {
-            console.log("Correct Answer!");
-        } else {
-            console.log("Wrong!");
-        }  
-    });
-
-    fourthAnswer.addEventListener("click", function(){
-        if (parseInt(fourthAnswer.getAttribute("data-index")) === choices.indexOf(correctanswer)) {
-            console.log("Correct Answer!");
-        } else {
-            console.log("Wrong!");
-        }  
-    });
+    if (parseInt(button.getAttribute("data-index")) === choices.indexOf(correctanswer)) {
+        showFeedback.textContent = "Correct!";
+        score += 1;
+    } else {
+        showFeedback.textContent = "Wrong!";
+        timeLeft -= 10; 
+    };
+    currentQuestionIndex++;
+    showQuestionsAndAnswers (timeLeft, questionsAndAnswers[currentQuestionIndex]);
 }
 
-function displayAnswers(test) {
-    console.log(test.answerChoices);
-}
+// function displayAnswers(test) {
+//     console.log(test.answerChoices);
+// }
 
-function displayCorrectAnswer(test) {
-    console.log(test.correctChoice);
-}
+// function displayCorrectAnswer(test) {
+//     console.log(test.correctChoice);
+// }
